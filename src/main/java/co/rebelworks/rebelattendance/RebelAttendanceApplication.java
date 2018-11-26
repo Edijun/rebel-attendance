846b083660a0c1ece0978ce1ce874a98a96950d9
edijun <edi.djunaidi@rebelworks.co>
2018-11-26 13:52:42 +0700

    first commit

---
+++ src/main/java/co/rebelworks/rebelattendance/RebelAttendanceApplication.java
@@ -0,0 +1,21 @@
+package co.rebelworks.rebelattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class RebelAttendanceApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebelAttendanceApplication.class, args);
+	}
+}
---
+++ src/test/java/co/rebelworks/rebelattendance/RebelAttendanceApplicationTests.java
@@ -0,0 +1,16 @@
+package co.rebelworks.rebelattendance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RebelAttendanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
